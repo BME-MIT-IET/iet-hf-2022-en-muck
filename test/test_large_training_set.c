473fc8a3cde0b4a4a9a1792c057462d9b4871739
Amani215 <amanibrik5@gmail.com>
2022-05-11 15:22:37 +0200

    Fixed: Compilation bugs

--- test/test_large_training_set.c
+++ test/test_large_training_set.c
@@ -1,20 +1,20 @@
-#include "../recommender.h"
+#include "../src/recommender.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
 #include <math.h>
 
-#include "../matrix_factorization.h"
-#include "../matrix_factorization_bias.h"
-#include "../sparse_matrix.h"
+#include "../src/matrix_factorization.h"
+#include "../src/matrix_factorization_bias.h"
+#include "../src/sparse_matrix.h"
 
 int main(void) {
 
 	learned_factors_t* learned;
 	training_set_t* tset;
 
-	learning_model_t model = {0};
+	learning_model_t* model = {0};
 
 	unsigned int i, j, k;
 
@@ -22,7 +22,7 @@ int main(void) {
 
 	//Model configuration
 	//Setup model parameters
-	model_parameters_t params;
+	model_parameters_t* params;
 
 	params->dimensionality = 100;
 	params->iteration_number = 1;
@@ -35,11 +35,11 @@ int main(void) {
 	//Use the basic matrix factorization model
 	model->learning_algorithm = learn_mf_bias;
 	model->rating_estimator   = estimate_rating_mf_bias;
-	model->parameters		 = params;
+	model->parameters		 = *params;
 
 	//Learning
 	//Initialize a training set
-	tset = init_training_set(&params);
+	tset = init_training_set(params);		//remove the & before the params
 
 	for (i = 0; i < 600; i++)
 	{
@@ -60,7 +60,7 @@ int main(void) {
 
 	for (k = 0; k < 100; k++)
 	{
-		update_learned_factors_mf_bias(learned, tset, &params);
+		update_learned_factors_mf_bias(learned, tset, params);		//remove & before params here too
 	}
 
 	sum = 0;
@@ -69,17 +69,45 @@ int main(void) {
 	{
 		for (j = 0; j < 100; j++)
 		{
-			sum += fabs(get_element(i, j, tset->ratings_matrix) -  estimate_rating_from_factors(i, j, learned, &model));
+			rating_estimator_parameters_t* temp = {0};
+
+			temp->user_index = i;
+			temp->item_index = j;
+			temp->lfactors = learned;
+
+			//sum += fabs(get_element(i, j, tset->ratings_matrix) -  estimate_rating_from_factors(i, j, learned, &model));
+			sum += fabs(get_element(i, j, tset->ratings_matrix) -  estimate_rating_from_factors(temp, model));
 		}
 	}
 
 	printf("Total error {%f} \n", sum/60000.0);
 
 	//Rating estimation
+	/*
 	printf("users [32] item [11], rating = %f \n", estimate_rating_from_factors(32, 11, learned, &model));
 	printf("users [190] item [3], rating = %f \n", estimate_rating_from_factors(190, 3, learned, &model));
 	printf("users [1] item [1], rating = %f \n",  estimate_rating_from_factors(1, 1, learned, &model));
 	printf("users [1] item [0], rating = %f \n", estimate_rating_from_factors(1, 0, learned, &model));
+	*/
+
+	rating_estimator_parameters_t* temp = {0};
+
+	temp->user_index = 32;
+	temp->item_index = 11;
+	temp->lfactors = learned;
+	printf("users [32] item [11], rating = %f \n", estimate_rating_from_factors(temp, model));
+
+	temp->user_index = 190;
+	temp->item_index = 3;
+	printf("users [190] item [3], rating = %f \n", estimate_rating_from_factors(temp, model));
+	
+	temp->user_index = 1;
+	temp->item_index = 1;
+	printf("users [1] item [1], rating = %f \n",  estimate_rating_from_factors(temp, model));
+	
+	temp->user_index = 1;
+	temp->item_index = 0;
+	printf("users [1] item [0], rating = %f \n", estimate_rating_from_factors(temp, model));
 
 	free_learned_factors(learned);
 	free_training_set(tset);
